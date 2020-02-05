2237edeb34dc58b1db47e5d1fd2c894bc96f097e
GarrettAnderson <garrettleegrahamanderson@gmail.com>
2020-02-05 09:40:22 -0500

    working on the js basics of the udemy course and following along.

--- public/main.js
+++ public/main.js
@@ -239,3 +239,221 @@ const whatDoYouDo = (job, firstName) => {
 console.log(whatDoYouDo('teacher', 'John'))
 console.log(whatDoYouDo('driver', 'Jane'))
 console.log(whatDoYouDo('retired', 'Mike'))
+
+/*********************************************
+ Arrays
+ */
+// Initialize new array
+let names = [ 'John', 'Mark', 'Jane' ]
+let years = new Array(1990, 1969, 1948)
+console.log(names[0])
+
+// Mutate array data
+names[1] = 'Ben'
+names[names.length] = 'Mary'
+console.log(names.length)
+
+var john = [ 'John', 'Smith', 1990, 'teacher', false ]
+john.push('blue') // push adds to end of the array
+john.unshift('Mr.') // adds to beginning of array
+john.pop() // removes value from the end of array
+john.shift() // removes the value from the beginning of the array
+
+console.log(john)
+
+console.log(john.indexOf(1990)) // returns -1 if no element in the array
+
+var isDesigner = john.indexOf('designer') === -1 ? 'John is NOT a designer' : 'John IS a designer'
+console.log(isDesigner)
+
+// Tip Calculator challenge
+const tipCalculator = (bill) => {
+  var percentage
+  if (bill < 50) {
+    percentage = 0.2
+  } else if (bill >= 50 && bill < 200) {
+    percentage = 0.15
+  } else {
+    percentage = 0.1
+  }
+  return percentage * bill
+}
+
+console.log(tipCalculator(100))
+var bills = [ 124, 48, 268 ]
+var tips = [ tipCalculator(bills[0]), tipCalculator(bills[1]), tipCalculator(bills[2]) ]
+var finalValues = [ bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2] ]
+
+console.log(tips, finalValues)
+
+/*******************************************
+ *Objects and properties
+ */
+
+var john = {
+  firstName: 'John',
+  lastName: 'Smith',
+  birthYear: 1989,
+  family: [ 'Jane', 'Mark', 'Emily' ],
+  job: 'teacher',
+  isMarried: false
+}
+
+console.log(john.firstName)
+console.log(john['lastName'])
+var x = 'birthYear'
+console.log(john[x])
+
+john.job = 'designer'
+john['isMarried'] = true
+console.log(john)
+
+var jane = new Object()
+jane.firstName = 'Jane'
+jane.birthYear = '1969'
+jane['lastName'] = 'Smith'
+console.log(jane)
+
+var john = {
+  firstName: 'John',
+  lastName: 'Smith',
+  birthYear: 1989,
+  family: [ 'Jane', 'Mark', 'Emily' ],
+  job: 'teacher',
+  isMarried: false,
+  calcAge: function() {
+    this.age = 2018 - this.birthYear
+  }
+}
+
+// console.log(john.calcAge())
+// john.age = john.calcAge()
+john.calcAge()
+console.log(john)
+
+var john = {
+  fullName: 'John Smith',
+  mass: 92,
+  height: 1.95,
+  calcBMI: function() {
+    this.bmi = this.mass / (this.height * this.height)
+    return this.bmi
+  }
+}
+
+var mark = {
+  fullName: 'Mark Jones',
+  mass: 78,
+  height: 1.69,
+  calcBMI: function() {
+    this.bmi = this.mass / (this.height * this.height)
+    return this.bmi
+  }
+}
+
+john.calcBMI()
+mark.calcBMI()
+console.log(john, mark)
+
+if (john.bmi > mark.bmi) {
+  console.log(john.fullName + ' has a higher bmi of ' + john.bmi)
+} else if (mark.bmi > john.bmi) {
+  console.log(mark.fullName + ' has a higher bmi of ' + mark.bmi)
+} else {
+  console.log('There is a tie')
+}
+
+/*********************************************
+ Loops and iteration
+ */
+
+for (var i = 0; i < 10; i++) {
+  console.log(i)
+}
+
+let james = [ 'James', 'Smith', 1990, 'designer', false, 'blue' ]
+/*
+for (var i = 0; i < james.length; i++) {
+  console.log(james[i])
+}
+*/
+// Continue and break statements
+/*
+for (var i = 0; i < james.length; i++) {
+  if (typeof john[i] !== 'string') continue
+  console.log(james[i])
+}
+
+for (var i = 0; i < james.length; i++) {
+  if (typeof john[i] !== 'string') break
+  console.log(james[i])
+}
+*/
+// Looping backwards
+for (var i = james.length - 1; i >= 0; i--) {
+  console.log(james[i])
+}
+
+// Coding challenge #5
+var johnDinner = {
+  fullName: 'John Smith',
+  bills: [ 124, 48, 268, 180, 42 ],
+  calcTips: function() {
+    this.tips = []
+    this.finalValues = []
+    for (var i = 0; i < this.bills.length; i++) {
+      var percentage
+      var bill = this.bills[i]
+      if (bill < 50) {
+        percentage = 0.2
+      } else if (bill > 50 && bill < 200) {
+        percentage = 0.15
+      } else {
+        percentage = 0.1
+      }
+      this.tips[i] = bill * percentage
+      this.finalValues[i] = bill + bill * percentage
+    }
+  }
+}
+
+johnDinner.calcTips()
+console.log(johnDinner)
+
+var markDinner = {
+  fullName: 'Mark Jones',
+  bills: [ 77, 375, 110, 45 ],
+  calcTips: function() {
+    this.tips = []
+    this.finalValues = []
+    for (var i = 0; i < this.bills.length; i++) {
+      var percentage
+      var bill = this.bills[i]
+      if (bill < 100) {
+        percentage = 0.2
+      } else if (bill > 100 && bill < 300) {
+        percentage = 0.1
+      } else {
+        percentage = 0.25
+      }
+      this.tips[i] = bill * percentage
+      this.finalValues[i] = bill + bill * percentage
+    }
+  }
+}
+
+const calcAverage = (tips) => {
+  const sum = 0
+  for (var i = 0; i < tips.length; i++) {
+    sum = sum + tips[i]
+  }
+  return sum / tips.length
+}
+
+markDinner.calcTips()
+console.log(markDinner)
+
+john.average = calcAverage(john.tips)
+mark.average = calcAverage(mark.tips)
+
+console.log(john, mark)
